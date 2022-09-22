1a17ec5e1719c10b4abcb1c0c1d143ca63071858
Johan Eriksson <johan.eriksson@ohmy.se>
2022-09-22 13:47:56 +0200

    Initial commit

---
+++ resources/js/app.js
@@ -0,0 +1,2 @@
+// Our main CSS
+import '../css/app.css'
---
+++ web/index.php
@@ -0,0 +1,13 @@
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>WithWithout - Frontend</title>
+    </head>
+    <body>
+
+
+        <script type="module" crossorigin src="http://localhost:3000/@vite/client"></script>
+        <script type="module" crossorigin src="http://localhost:3000/resources/js/app.js"></script>
+    </body>
+</html>
